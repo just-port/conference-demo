5b7de6ad020851391206d46f6a462bdb3aba786e
Justin Portell <72322829+just-port@users.noreply.github.com>
2022-06-22 22:09:39 -0500

    WIP

--- src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,8 +1,8 @@
 package com.pluralsight.conferencedemo.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "sessions")
@@ -16,36 +16,5 @@ public class Session {
 
   public Session() {
   }
-  
-  public Long getSession_id() {
-    return session_id;
-  }
 
-  public void setSession_id(Long session_id) {
-    this.session_id = session_id;
-  }
-
-  public String getSession_name() {
-    return session_name;
-  }
-
-  public void setSession_name(String session_name) {
-    this.session_name = session_name;
-  }
-
-  public String getSession_description() {
-    return session_description;
-  }
-
-  public void setSession_description(String session_description) {
-    this.session_description = session_description;
-  }
-
-  public Integer getSession_length() {
-    return session_length;
-  }
-
-  public void setSession_length(Integer session_length) {
-    this.session_length = session_length;
-  }
 }
