aecec2531bda00b8bf38a1fa29d0daa752684911
Justin Portell <72322829+just-port@users.noreply.github.com>
2022-08-14 12:11:45 -0500

    Configurations lessons

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -16,55 +16,57 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionsController {
 
-  private final SessionRepository sessionRepository;
+    private final SessionRepository sessionRepository;
 
-  public SessionsController(SessionRepository sessionRepository) {
-    this.sessionRepository = sessionRepository;
-  }
-
-  @GetMapping
-  public List<Session> list() {
-    return sessionRepository.findAll();
-  }
-
-  @GetMapping
-  @RequestMapping("{id}")
-  public Session findById(@PathVariable Long id) {
-    Optional<Session> opt;
-    opt = sessionRepository.findById(id);
-    if (opt.isEmpty()) {
-      throw new NoSuchElementException("Session not found");
+    public SessionsController(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
     }
-    return opt.get();
-  }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Session create(@RequestBody final Session session) {
-    return sessionRepository.saveAndFlush(session);
-  }
-
-  @DeleteMapping("{id}")
-  public void delete(@PathVariable Long id) {
-    // Also need to check for children records before deleting
-    sessionRepository.deleteById(id);
-  }
-
-  @PutMapping("{id}")
-  public Session update(@PathVariable Long id, @RequestBody Session session) {
-    // TODO: add validation that all attributes are passed in, otherwise return a 400 bad payload
-    Optional<Session> opt;
-    opt = sessionRepository.findById(id);
-    if (opt.isEmpty()) {
-      throw new NoSuchElementException("Session not found");
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
     }
-    var exisitingSession = opt.get();
-    BeanUtils.copyProperties(session, exisitingSession, "session_id");
-    return sessionRepository.saveAndFlush(exisitingSession);
-  }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session findById(@PathVariable Long id) {
+        Optional<Session> opt;
+        opt = sessionRepository.findById(id);
+        if (opt.isEmpty()) {
+            throw new HttpStatusCodeException(HttpStatus.NOT_FOUND) {
+            };
+        }
+        return opt.get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting
+        sessionRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // TODO: add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Optional<Session> opt;
+        opt = sessionRepository.findById(id);
+        if (opt.isEmpty()) {
+            throw new NoSuchElementException("Session not found");
+        }
+        var existingSession = opt.get();
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
 }
--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -3,7 +3,6 @@ package com.pluralsight.conferencedemo.controllers;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -16,55 +15,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 
 @RestController
 @RequestMapping("/api/v1/speakers")
 public class SpeakerController {
 
-  private final SpeakerRepository speakerRepository;
+    private final SpeakerRepository speakerRepository;
 
-  public SpeakerController(SpeakerRepository speakerRepository) {
-    this.speakerRepository = speakerRepository;
-  }
-
-  @GetMapping
-  public List<Speaker> list() {
-    return speakerRepository.findAll();
-  }
-
-  @GetMapping
-  @RequestMapping("{id}")
-  public Speaker findById(@PathVariable Long id) {
-    Optional<Speaker> opt;
-    opt = speakerRepository.findById(id);
-    if (opt.isEmpty()) {
-      throw new NoSuchElementException("Speaker not found");
+    public SpeakerController(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
     }
-    return opt.get();
-  }
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Speaker create(@RequestBody final Speaker speaker) {
-    return speakerRepository.saveAndFlush(speaker);
-  }
-
-  @DeleteMapping("{id}")
-  public void delete(@PathVariable Long id) {
-    // Also need to check for children records before deleting
-    speakerRepository.deleteById(id);
-  }
-
-  @PutMapping("{id}")
-  public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
-    // TODO: add validation that all attributes are passed in, otherwise return a 400 bad payload
-    Optional<Speaker> opt;
-    opt = speakerRepository.findById(id);
-    if (opt.isEmpty()) {
-      throw new NoSuchElementException("Speaker not found");
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
     }
-    var existingSpeaker = opt.get();
-    BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
-    return speakerRepository.saveAndFlush(existingSpeaker);
-  }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker findById(@PathVariable Long id) {
+        Optional<Speaker> opt;
+        opt = speakerRepository.findById(id);
+        if (opt.isEmpty()) {
+            throw new HttpStatusCodeException(HttpStatus.NOT_FOUND) {
+            };
+        }
+        return opt.get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        // Also need to check for children records before deleting
+        speakerRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        // TODO: add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Optional<Speaker> opt;
+        opt = speakerRepository.findById(id);
+        if (opt.isEmpty()) {
+            throw new HttpStatusCodeException(HttpStatus.NOT_FOUND) {
+            };
+        }
+        var existingSpeaker = opt.get();
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
 }
