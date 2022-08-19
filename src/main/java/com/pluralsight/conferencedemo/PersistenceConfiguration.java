2b7da31414aca87e794ff9cc1309d8351f85af7b
Justin Portell <72322829+just-port@users.noreply.github.com>
2022-08-19 16:03:37 -0500

    more adjustin

--- src/main/java/com/pluralsight/conferencedemo/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/PersistenceConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_demo");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url(DATABASE_URL);
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
 }
